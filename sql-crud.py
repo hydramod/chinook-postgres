5ed0c0549c76247576039aca6c158232846369cb
hydramod <>
2023-05-23 17:29:19 +0000

    Add sql-crud.py with some queries, add progammer table and records

---
+++ sql-crud.py
@@ -0,0 +1,103 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# execute instruction from localhost "chinook" db
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+# create new table "Programmer"
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+
+Session = sessionmaker(db)
+# Opens session
+session = Session()
+
+# creat database using declarative_base subclass
+base.metadata.create_all(db)
+
+
+# create new record on our programmer table
+ada_lovelace = Programmer(
+    first_name = "Ada",
+    last_name = "Lovelace",
+    gender = "F",
+    nationality = "British",
+    famous_for = "First programmer"
+)
+
+alan_turing = Programmer(
+    first_name = "Alan",
+    last_name = "Turing",
+    gender = "M",
+    nationality = "British",
+    famous_for = "Modern computing"
+)
+
+grace_hopper = Programmer(
+    first_name = "Grace",
+    last_name = "Hopper",
+    gender = "F",
+    nationality = "American",
+    famous_for = "COBOL Language"
+)
+
+margaret_hamilton = Programmer(
+    first_name = "Margaret",
+    last_name = "Hamilton",
+    gender = "F",
+    nationality = "American",
+    famous_for = "Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name = "Bill",
+    last_name = "Gates",
+    gender = "M",
+    nationality = "American",
+    famous_for = "Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name = "Tim",
+    last_name = "Berners-Lee",
+    gender = "M",
+    nationality = "British",
+    famous_for = "World Wide Web"
+)
+
+# add each instance to uor session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+
+
+# commit the session
+#session.commit()
+
+#query the db to find all programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
